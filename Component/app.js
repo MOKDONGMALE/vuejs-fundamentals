fd783468aae3ed078594185109a2bdfdd7bee3d1
MOKDONGMALE <naviworks6@gmail.com>
2020-06-17 18:01:58 +0900

    Vue2

--- Component/app.js
+++ Component/app.js
@@ -1,24 +1,53 @@
-Vue.component('plan-picker', {
-    template: '#plan-picker-template',
-    data() {
-        return {
-            plans: ['The Single', 'The Curious', 'The Addict']
-        } 
-    }
-})
-
-
-Vue.component('plan', {
+let PlanComponent = {
     template: '#plan-template',
     props: {
-      name: {
-        type: String,
-        required: true
-      }
+        name: {
+            type: String,
+            required: true
+        },
+        selectedPlan: {
+                type: String
+            }
+        },
+        computed:{
+            isSelected() {
+                return this.name == this.selectedPlan
+            }
+        },
+        methods: {
+            select() {
+                this.$emit('select', this.name)
+            
+            }
+        }
+}
+
+
+let PlanPickerComponent = {
+    template: '#plan-picker-template',
+    components:{
+        plan: PlanComponent
+    },
+    data() {
+        return {
+            plans: ['The Single', 'The Curious', 'The Addict'],
+            selectedPlan :null
+        }
+    },
+    methods: {
+        selectPlan(plan) {
+            this.selectedPlan = plan
+        }
     }
-  })
-  
-  new Vue({
-    el: '#app'
-    
-  }) 
+}
+
+
+
+
+new Vue({
+    el: '#app',
+    components: {
+        'plan-picker': PlanPickerComponent
+    }
+
+}) 
